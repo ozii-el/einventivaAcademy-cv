ca9f538c75862b21770cb98b65c0c1f7a77f4dcb
Administrator <gitlab_admin_1c9a49@example.com>
2025-04-07 18:37:14 -0600

    se agrega la tareaa de js

---
+++ Views/javascript/km.js
@@ -0,0 +1,30 @@
+function calcularConsumo() {
+let KM = prompt("🚘 ¿Kilometros recorridos de tu carro? 🚘");
+let Lts = prompt("📉 ¿Número de litros consumidos? 📉");
+
+let Gas = Lts/KM;
+
+let gasto = Gas.toFixed(2);
+
+
+alert('💸💸 Tu carro gasta: ' + gasto + ' litros por cada kilometro 💸💸');
+};
+
+
+function cambioUnidades() {
+let hora = prompt("⌚Dame la hora a convertir a segundos⌚");
+let horaSplit = hora.split(":");
+
+let horas = horaSplit[0];
+let minutos = horaSplit[1];
+
+//alert("Formato separado: horas: " + horas + " minutos:" + minutos )
+
+let horasMin = parseInt(horas) * 60; 
+let minTotal = parseInt(horasMin) + parseInt(minutos);
+let minSeg = parseInt(minTotal) * 60;
+
+
+alert("🕑" + hora + " horas, es igual a " + minSeg + " segundos 🕑")
+
+};
